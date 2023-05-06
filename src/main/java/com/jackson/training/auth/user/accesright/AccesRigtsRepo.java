a7c482b9fb9512a12e4f5da8a30e4e3f504b57bc
jackson05 <jackson5g.bi@gmail.com>
2023-05-06 22:28:44 +0200

    inheritance of accesrights to jpaRepo

---
+++ src/main/java/com/jackson/training/auth/user/accesright/AccesRigtsRepo.java
@@ -0,0 +1,7 @@
+package com.jackson.training.auth.user.accesright;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccesRigtsRepo extends JpaRepository<AccesRights,Integer> {
+    
+}
