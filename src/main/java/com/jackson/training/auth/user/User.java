1f14bf9cf338de2289ff859ad29d785038a5f650
jackson05 <jackson5g.bi@gmail.com>
2023-05-12 23:24:48 +0200

    implementation to userDetail

--- src/main/java/com/jackson/training/auth/user/User.java
+++ src/main/java/com/jackson/training/auth/user/User.java
@@ -1,8 +1,12 @@
 package com.jackson.training.auth.user;
 
+import java.util.Collection;
 import java.util.Date;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
 
     private int id;
     private String name;
@@ -88,6 +92,36 @@ public class User {
     }
     public void setRecordDate(Date recordDate) {
         this.recordDate = recordDate;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
+    }
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
     }
 
 
