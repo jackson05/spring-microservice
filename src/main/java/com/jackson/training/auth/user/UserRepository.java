86ee269c17f3cfee57d872541ee5143c54e31be4
jackson05 <jackson5g.bi@gmail.com>
2023-05-15 23:23:33 +0200

    created abstract methode that handle user By email

--- src/main/java/com/jackson/training/auth/user/UserRepository.java
+++ src/main/java/com/jackson/training/auth/user/UserRepository.java
@@ -3,5 +3,5 @@ package com.jackson.training.auth.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-    
+    public User findUserByEmail();
 }
