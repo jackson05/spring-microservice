0fa301451ad92a3358c2b914ae6ab6a8f0813554
jackson05 <jackson5g.bi@gmail.com>
2023-05-18 23:43:41 +0200

    the changes happens between user and userService

--- src/main/java/com/jackson/training/auth/user/UserService.java
+++ src/main/java/com/jackson/training/auth/user/UserService.java
@@ -1,15 +1,19 @@
 package com.jackson.training.auth.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserService implements UserDetailsService{
+    
+    @Autowired
+    private UserRepository userrepo;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+    public UserDetails loadUserByUsername(String username)  {
+        User user=userrepo.findUserByEmail();
+        return user;
     }
     
 }
